f1ce4d36059dcb8b21a6e6771cab4f61695e5700
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-07-30 22:05:30 +0300

    Add Save methods defenition to DataContext interface

--- Blog.Application/Contract/Interfaces/IDataContext.cs
+++ Blog.Application/Contract/Interfaces/IDataContext.cs
@@ -7,4 +7,8 @@ public interface IDataContext
 	public DbSet<PostItem> Posts { get; set; }
 	public DbSet<Comment> Comments { get; set; }
 	public DbSet<Tag> Tags { get; set; }
+	public int SaveChanges();
+	public int SaveChanges(bool acceptAllChangesOnSucces);
+	public Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+	public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
