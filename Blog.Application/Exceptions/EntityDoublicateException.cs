0f3b35bc18a0a16e37831bec4e0a99ef625ff730
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-07-30 22:07:32 +0300

    Exceptions, used by commands

---
+++ Blog.Application/Exceptions/EntityDoublicateException.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog.Application.Exceptions
+{
+	internal class EntityDoublicateException : Exception
+	{
+		public string EntityName { get; }
+		public object DoublicateKey { get; }
+		public EntityDoublicateException(string entityName, object doublicateKey)
+			: base($"Entity doublicate. Entity name - \"{entityName}\". Doublicate key - \"{doublicateKey}\"")
+		{
+			EntityName = entityName;
+			DoublicateKey = doublicateKey;
+		}
+
+	}
+}
---
+++ Blog.Application/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.Application.Exceptions
+{
+	public class EntityNotFoundException : Exception
+	{
+		public string EntityName { get; }
+		public object Key { get; }
+		public EntityNotFoundException(string entityName, object key) : base($"Entity \"{entityName}\" was not found by key - \"{key}\"")
+		{
+			EntityName = entityName;
+			Key = key;
+		}
+	}
+}
