a5f4ae6454c58cfd6ce6237cd502912ff00684a1
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-07-30 22:06:37 +0300

    Impliment Create Tag Command

--- Blog.Application/CQRS/Tag/Commands/Create/CreateCommandHandler.cs
+++ Blog.Application/CQRS/Tag/Commands/Create/CreateCommandHandler.cs
@@ -1,12 +1,26 @@
-﻿using MediatR;
+﻿using Blog.Application.Contract.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Application.CQRS.Tag.Commands.Create
 {
 	public class CreateCommandHandler : IRequestHandler<CreateCommand, Guid>
 	{
-		public Task<Guid> Handle(CreateCommand request, CancellationToken cancellationToken)
+
+		private readonly IDataContext _dataContext;
+		public CreateCommandHandler(IDataContext dataContext)
 		{
-			throw new NotImplementedException();
+			_dataContext = dataContext;
+		}
+		public async Task<Guid> Handle(CreateCommand request, CancellationToken cancellationToken)
+		{
+			Model.Tag tag = await _dataContext.Tags.FirstOrDefaultAsync(x => x.Name.Equals(request.Name, StringComparison.CurrentCultureIgnoreCase), cancellationToken);
+			if (tag != null)
+				throw new EntityDoublicateException(nameof(Model.Tag), tag.Id);
+			tag = new Model.Tag() { Id = Guid.NewGuid(), Name = request.Name };
+			var result = await _dataContext.Tags.AddAsync(tag,cancellationToken);
+			await _dataContext.SaveChangesAsync(cancellationToken);
+			return result.Entity.Id;
 		}
 	}
 }
