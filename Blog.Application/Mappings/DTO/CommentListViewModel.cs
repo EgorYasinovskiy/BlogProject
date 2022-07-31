106a975dd8066f372c4612f775b8d5156f64c80e
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-08-01 00:12:00 +0300

    Add DTO for comments

---
+++ Blog.Application/Mappings/DTO/CommentListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog.Application.Mappings.DTO
+{
+	public class CommentListViewModel
+	{
+		public ICollection<CommentViewModel> Comments { get; set; }
+	}
+}
---
+++ Blog.Application/Mappings/DTO/CommentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Blog.Application.Contract.Interfaces;
+
+namespace Blog.Application.Mappings.DTO
+{
+	public class CommentViewModel : IMapWith<Model.Comment>
+	{
+		public string UserName { get; set; }
+		public string Comment { get; set; }
+		public DateTime Created { get; set; }
+
+		public void Mapping(Profile profile)
+		{
+			profile.CreateMap<Model.Comment, CommentViewModel>()
+				.ForMember(vm => vm.UserName, opt => opt.MapFrom(mod => mod.Author.Username))
+				.ForMember(vm => vm.Comment, opt => opt.MapFrom(mod => mod.Text))
+				.ForMember(vm => vm.Created, opt => opt.MapFrom(mod => mod.Created));
+		}
+	}
+}
