35badf7ff534a8e190a37b033a04d04d9de60397
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-08-06 18:43:51 +0300

    Delete post command implimentation

---
+++ Blog.Application/CQRS/Post/Commands/Delete/DeleteCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Blog.Application.CQRS.Post.Commands.Delete
+{
+	public class DeleteCommand : IRequest
+	{
+		public Guid PostId { get; set; }
+	}
+}
---
+++ Blog.Application/CQRS/Post/Commands/Delete/DeleteCommandHadler.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Application.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.CQRS.Post.Commands.Delete
+{
+	public class DeleteCommandHadler : IRequestHandler<DeleteCommand>
+	{
+		private readonly IDataContext _context;
+
+		public DeleteCommandHadler(IDataContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<Unit> Handle(DeleteCommand request, CancellationToken cancellationToken)
+		{
+			var post = await _context.Posts.FindAsync(request.PostId, cancellationToken);
+			if (post == null)
+				throw new EntityNotFoundException(nameof(Model.PostItem), request.PostId);
+			_context.Posts.Remove(post);
+			await _context.SaveChangesAsync();
+			return Unit.Value;
+		}
+	}
+}
