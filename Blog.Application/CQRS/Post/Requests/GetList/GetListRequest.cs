5d8c2fd430b4f748a69ca35642152a9c6d8616a1
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-08-06 18:11:47 +0300

    Get all posts requests implimentation

---
+++ Blog.Application/CQRS/Post/Requests/GetList/GetListRequest.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Application.Mappings.DTO;
+using MediatR;
+
+namespace Blog.Application.CQRS.Post.Requests.GetList
+{
+	public class GetListRequest : IRequest<PostListViewModel>
+	{
+
+	}
+}
---
+++ Blog.Application/CQRS/Post/Requests/GetList/GetListRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Blog.Application.Mappings.DTO;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Application.CQRS.Post.Requests.GetList
+{
+	public class GetListRequestHandler : IRequestHandler<GetListRequest, PostListViewModel>
+	{
+		private readonly IDataContext _dataContext;
+		private readonly IMapper _mapper;
+
+		public GetListRequestHandler(IDataContext dataContext, IMapper mapper)
+		{
+			_dataContext = dataContext;
+			_mapper = mapper;
+		}
+		public async Task<PostListViewModel> Handle(GetListRequest request, CancellationToken cancellationToken)
+		{
+			var posts = await _dataContext.Posts.ProjectTo<PostListItemViewModel>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+			return new PostListViewModel() { Posts = posts };
+		}
+	}
+}
