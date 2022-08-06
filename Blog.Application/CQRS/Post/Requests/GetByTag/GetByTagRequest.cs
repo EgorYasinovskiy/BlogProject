846d6b50fdaf6d01b4111c146a46b42898b8969e
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-08-06 18:11:33 +0300

    Get list of posts by tag request implimentation

---
+++ Blog.Application/CQRS/Post/Requests/GetByTag/GetByTagRequest.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Application.Mappings.DTO;
+using MediatR;
+
+namespace Blog.Application.CQRS.Post.Requests.GetByTag
+{
+	public class GetByTagRequest : IRequest<PostListViewModel>
+	{
+		public Guid TagId { get; set; }
+	}
+}
---
+++ Blog.Application/CQRS/Post/Requests/GetByTag/GetByTagRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Blog.Application.Mappings.DTO;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Application.CQRS.Post.Requests.GetByTag
+{
+	public class GetByTagRequestHandler : IRequestHandler<GetByTagRequest, PostListViewModel>
+	{
+		private readonly IDataContext _dataContext;
+		private readonly IMapper _mapper;
+
+		public GetByTagRequestHandler(IDataContext dataContext, IMapper mapper)
+		{
+			_dataContext = dataContext;
+			_mapper = mapper;
+		}
+		public async Task<PostListViewModel> Handle(GetByTagRequest request, CancellationToken cancellationToken)
+		{
+			var posts = await _dataContext.Posts
+				.Where(x=>x.Tags.Any(y=>y.Id==request.TagId))
+				.ProjectTo<PostListItemViewModel>(_mapper.ConfigurationProvider)
+				.ToListAsync(cancellationToken);
+			return new PostListViewModel() { Posts = posts };
+		}
+	}
+}
