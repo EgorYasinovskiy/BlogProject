3ee41cac063eff5af0410caa502587bda8a33bdb
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-08-06 18:11:17 +0300

    Get post request implimentation

---
+++ Blog.Application/CQRS/Post/Requests/Get/GetRequest.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Application.Mappings.DTO;
+using MediatR;
+
+namespace Blog.Application.CQRS.Post.Requests.Get
+{
+	public class GetRequest : IRequest<PostViewModel>
+	{
+		public Guid ID { get; set; }
+	}
+}
---
+++ Blog.Application/CQRS/Post/Requests/Get/GetRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Blog.Application.Exceptions;
+using Blog.Application.Mappings.DTO;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Application.CQRS.Post.Requests.Get
+{
+	public class GetRequestHandler : IRequestHandler<GetRequest, PostViewModel>
+	{
+		private readonly IDataContext _dataContext;
+		private readonly IMapper _mapper;
+
+		public GetRequestHandler(IDataContext dataContext, IMapper mapper)
+		{
+			_dataContext = dataContext;
+			_mapper = mapper;
+		}
+		public async Task<PostViewModel> Handle(GetRequest request, CancellationToken cancellationToken)
+		{
+			var post = await _dataContext.Posts.FirstOrDefaultAsync(x => x.Id == request.ID,cancellationToken);
+			if (post == null)
+				throw new EntityNotFoundException(nameof(Model.PostItem), request.ID);
+			var dto = _mapper.Map<PostViewModel>(post);
+			return dto;
+
+		}
+	}
+}
