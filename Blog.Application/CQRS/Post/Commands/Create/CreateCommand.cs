6cdb3b36dafeaad2c0d7b8a80737b086dc7124ff
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-08-09 02:32:07 +0300

    Implimet create post command

---
+++ Blog.Application/CQRS/Post/Commands/Create/CreateCommand.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Application.Mappings.DTO;
+using MediatR;
+
+namespace Blog.Application.CQRS.Post.Commands.Create
+{
+	public class CreateCommand : IRequest<PostViewModel>
+	{
+		public Guid AuthorId { get; set; } 
+		public string Title { get; set; }
+		public string MarkDown { get; set; }
+		public IEnumerable<string> Tags { get; set; }
+
+	}
+}
---
+++ Blog.Application/CQRS/Post/Commands/Create/CreateCommandHadler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Blog.Application.Mappings.DTO;
+using Blog.Model;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Application.CQRS.Post.Commands.Create
+{
+	public class CreateCommandHadler : IRequestHandler<CreateCommand,PostViewModel>
+	{
+		private readonly IDataContext _context;
+		private readonly IMapper _mapper;
+
+		public CreateCommandHadler(IDataContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+
+		public async Task<PostViewModel> Handle(CreateCommand request, CancellationToken cancellationToken)
+		{
+			var tagsSet = request.Tags.Select(x=>x.ToLower()).ToHashSet();
+			var tags = _context.Tags.Where(x => tagsSet.Contains(x.Name.ToLower()));
+			var post = new PostItem()
+			{
+				AuthorId = request.AuthorId,
+				Title = request.Title,
+				MarkDown = request.MarkDown,
+				Tags = await tags.ToListAsync()
+			};
+			await _context.Posts.AddAsync(post, cancellationToken);
+			return _mapper.Map<PostViewModel>(post);
+		}
+	}
+}
