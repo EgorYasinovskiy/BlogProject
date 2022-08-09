68ac5bcefb6a6f2e53c4dcd916eb9d7f57b904de
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-08-10 01:03:08 +0300

    Change comment command implimentation

---
+++ Blog.Application/CQRS/Comment/Commands/Change/ChangeCommand.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Application.Mappings.DTO;
+using MediatR;
+
+namespace Blog.Application.CQRS.Comment.Commands.Change
+{
+	public class ChangeCommand : IRequest<CommentViewModel>
+	{
+		public Guid CurrentUserId { get; set; }
+		public Guid Id { get; set; }
+		public string NewText { get; set; }
+	}
+}
---
+++ Blog.Application/CQRS/Comment/Commands/Change/ChangeCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Blog.Application.Exceptions;
+using Blog.Application.Mappings.DTO;
+using MediatR;
+
+namespace Blog.Application.CQRS.Comment.Commands.Change
+{
+	public class ChangeCommandHandler : IRequestHandler<ChangeCommand, CommentViewModel>
+	{
+		private readonly IDataContext _context;
+		private readonly IMapper _mapper;
+
+		public ChangeCommandHandler(IDataContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+		public async Task<CommentViewModel> Handle(ChangeCommand request, CancellationToken cancellationToken)
+		{
+			var comment = await _context.Comments.FindAsync(request.Id,cancellationToken);
+
+			if (comment == null)
+				throw new EntityNotFoundException(nameof(Model.Comment), request.Id);
+			if (comment.AuthorID != request.CurrentUserId)
+				throw NoRightsGenerator.ChangeException(request.Id, request.CurrentUserId);
+
+			comment.Text = request.NewText;
+			_context.Comments.Update(comment);
+			await _context.SaveChangesAsync(cancellationToken);
+
+			return _mapper.Map<CommentViewModel>(comment);
+		}
+	}
+}
