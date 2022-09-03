b89dd77878d7c1dd47e8befbf0b520b617b3f243
yasinovskiy.e <yasinovskiy.e@digdes.com>
2022-09-04 02:22:11 +0300

    Add two new fileds and rename ID to Id

--- Blog.Model/User.cs
+++ Blog.Model/User.cs
@@ -2,9 +2,11 @@
 {
 	public class User
 	{
-		public Guid ID { get; set; }
+		public Guid Id { get; set; }
 		public UserRole Role { get; set; }
-		public string Username { get; set; }
+		public string UserName { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
 		public string Email { get; set; }
 		public bool Gender { get; set; }
 		public string Bio { get; set; }
